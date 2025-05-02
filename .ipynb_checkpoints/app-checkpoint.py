91287514194782c05a2d00737e903775a063dfb2
yashodhakumari <yashodha.kumari@spoors.in>
2025-05-02 15:41:20 +0530

    updated app.py

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -33,5 +33,5 @@ def save_template():
     return jsonify({"message": "Template saved successfully!"})
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 5000))  
+    port = int(os.environ.get('PORT', 10000))  
     app.run(host='0.0.0.0', port=port, debug=True)
